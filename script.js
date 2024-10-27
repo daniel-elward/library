6c11612e8bf9912d05699f849c905291b28ada5f
LR18 <daniel.elward@hotmail.com>
2024-10-27 01:01:23 -0300

    remove and toggle read
    added a remove button to delete a book from the library.
    added a read toggle button to change the read status of a book.

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function openForm() {
   }
 
 myLibrary = [
-  {image: "book-placeholder.jpg", title: "The Lord of the Rings", author: "John R.R Tolkien", pageCount: 423, read: "No"},
-  {image: "book-placeholder.jpg", title: "IT", author: "Stephen King", pageCount: 1138, read: "Yes"}
+  {image: "book-placeholder.jpg", title: "The Lord of the Rings", author: "John R.R Tolkien", pageCount: 423, read: "No", id: 0},
+  {image: "book-placeholder.jpg", title: "IT", author: "Stephen King", pageCount: 1138, read: "Yes", id: 1}
 ];
 
 function book(title, author, pageCount, read) {
+
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
     this.read = read;
+    this.id = myLibrary.length; //increments ID
+    
 
     this.info = function(){
         console.log(`${this.title} by ${this.author}, ${pageCount} pages, ${read}`)
@@ -35,15 +38,24 @@ function displayBooks (){
 
       createDiv.classList.add('display-card');
       createDiv.innerHTML = 
-      `<div class="thumbnail">
-        <img src="img/book-placeholder.jpg" alt="${myLibrary[i].title}" width="150px" height="200px">
-      </div>
-      <div class="book-info">
-        <div class="title"><p><b>Title:</b> ${myLibrary[i].title}</p></div>
-          <div class="author"><p><b>Author:</b> ${myLibrary[i].author}</p></div>
-            <div class="page-count"><p><b>Page Count:</b> ${myLibrary[i].pageCount}</p></div>
-              <div class="read"><p><b>Read Book?</b> ${myLibrary[i].read}</p></div>
-      </div>`
+        `<div class="thumbnail">
+            <img src="img/book-placeholder.jpg" alt="${myLibrary[i].title}" width="150px" height="200px">
+        </div>
+
+        <div class="book-info">
+          <div class="title"><p><b>Title:</b> ${myLibrary[i].title}</p></div>
+            <div class="author"><p><b>Author:</b> ${myLibrary[i].author}</p></div>
+              <div class="page-count"><p><b>Page Count:</b> ${myLibrary[i].pageCount}</p></div>
+                <div class="read"><p><b>Read Book?</b> ${myLibrary[i].read}</p></div>
+                <p>ID Number: ${myLibrary[i].id}</p>
+        </div>
+        
+        <div class="removeBtn">
+          <button class="removeBook" onclick="removeBook(this.value)" value="${myLibrary[i].id}">Remove Book</button>
+        </div>
+        <div class="toggleRead">
+          <button id="toggleRead" onclick="toggleRead(this.value)" value="${myLibrary[i].id}">Change Read Status</button>
+        </div>`
       getCardWrapper.append(createDiv);  
     }
 }
@@ -53,14 +65,46 @@ const newBookForm = document.querySelector('form');
 newBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newBookData = new FormData(newBookForm);
-  const bookObj = Object.fromEntries(newBookData);
+  const bookObj = Object.fromEntries(newBookData); 
 
-  //const newEntry = new book(tile, author, pageCount, read);
+  let addBook = new book(bookObj.thumbnail, bookObj.title, bookObj.author, bookObj.pageCount, bookObj.read, bookObj.id)
 
-  myLibrary.push({image: "book-placeholder.jpg", title: `${bookObj.title}`, author: `${bookObj.author}`, pageCount: bookObj.pageCount, read: bookObj.read})
+  /*
+  THIS WORKS - INCLUDING ID AND TITLE. I WANT TO USE THE OBJ CONSTRUCTOR THOUGH
+  myLibrary.push({image: "book-placeholder.jpg", title: `${bookObj.title}`, author: `${bookObj.author}`, pageCount: bookObj.pageCount, read: bookObj.read, id: myLibrary.length})
+  */
+
+  myLibrary.push({image: "book-placeholder.jpg", title: `${bookObj.title}`, author: `${bookObj.author}`, pageCount: bookObj.pageCount, read: bookObj.read, id: myLibrary.length})
+
+  //myLibrary.push(addBook);
 
   displayBooks()
 })
+
+function removeBook(buttonValue) {
+  let key = "id";
+  let value = buttonValue;
+  let indexToRemove = myLibrary.findIndex(elem => elem[key] == value);
+
+  myLibrary.splice(indexToRemove, 1);
+  displayBooks()
+}
+
+function toggleRead(buttonValue){
+  let bookID = "id";
+  let value = buttonValue;
+  let objIndex = myLibrary.findIndex(elem => elem[bookID] == value);
+
+  console.log(objIndex)
+
+  if (myLibrary[objIndex].read === "Yes") {
+    myLibrary[objIndex].read = "No"
+  } else if (myLibrary[objIndex].read === "No") {
+    myLibrary[objIndex].read = "Yes"
+  };
+  
+  displayBooks();
+}
 
 //initial display when first loaded
 displayBooks()
